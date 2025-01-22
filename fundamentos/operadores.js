386e09d64e227758a1414df9bcb0b60c16d7073c
LuisGustavoRSilva <luisgustavo050705@gmail.com>
2025-01-22 17:14:00 -0300

    tutorial

---
+++ fundamentos/operadores.js
@@ -0,0 +1,32 @@
+/**
+ * operadores
+ * aritmedicos
+ * aritmetricos de atribuição
+ * comparação
+ * logicos
+ */
+console.clear()
+console.log("Operadores aritmedicos")
+console.log("+ | Soma")
+console.log("- | Subtração")
+console.log("* | Multiplicação")
+console.log("/ | Divisão")
+console.log("% | Modulo(Resto)")
+console.log("")
+console.log("Operadores aritmedicos de atribuição")
+console.log("+= | Soma e atribuição a variavel")
+console.log("-= | Subtração e atribuição a variavel")
+console.log("*= | Multiplicação e atribuição a variavel")
+console.log("/= | Divisão e atribuição a variavel")
+console.log("*= | Multiplicação e atribuição a variavel")
+console.log("/= | Divisão e atribuição a variavel")
+console.log("++ | Somar 1 a variavel")
+console.log("-- | Substituir 1 na variavel")
+console.log("")
+console.log("Comparadores")
+console.log("> | Maior")
+console.log("< | Menos")
+console.log(">= | Maior ou igual")
+console.log("<= | Menor ou igual")
+console.log("=== | Idêntico")
+console.log("!== | não identico")
